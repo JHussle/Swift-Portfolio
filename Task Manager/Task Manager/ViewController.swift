db1284ffdcd8d140e11429a2d40c132585bbb018
JSmith <65369149+JHussle@users.noreply.github.com>
2020-10-30 14:30:59 -0500

    Add files via upload

---
+++ Task Manager/Task Manager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Task Manager
+//
+//  Created by Joey Smith on 10/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func darkMode(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
---
+++ The Night Porter/The Night Porter/The_Night_PorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Night_PorterApp.swift
+//  The Night Porter
+//
+//  Created by Joey Smith on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct The_Night_PorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
